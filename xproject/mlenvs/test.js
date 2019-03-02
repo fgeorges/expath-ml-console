c23912173bbc492ab87e8a302604295fd574fbf9
Florent Georges <fgeorges@fgeorges.org>
2019-03-02 17:17:54 +0100

    Update the Star Wars test data set.

--- xproject/mlenvs/test.js
+++ xproject/mlenvs/test.js
@@ -1,6 +1,28 @@
 // To be used in conjunction with another environment, for the connect info:
 //
 //     > mlproj -e dev/test load -b test
+//
+// Use the following query in QConsole to transform Turtle files to MarkLogic
+// sem:triples documents:
+//
+//     'use strict';
+//
+//     declareUpdate();
+//
+//     const sem = require('/MarkLogic/semantics.xqy');
+//
+//     const res = [];
+//     for ( const uri of cts.uris() ) {
+//       const u = uri.toString();
+//       if ( u.startsWith('/star-wars-dataset/') && u.endsWith('.ttl') ) {
+//         const n = u.slice(0, -3) + 'xml';
+//         const c = sem.rdfSerialize(sem.rdfParse(fn.doc(u), 'turtle'), 'triplexml');
+//         xdmp.documentInsert(n, c);
+//         res.push(n);
+//       }
+//     }
+//     res;
+
 
 module.exports = function() {
     return {
