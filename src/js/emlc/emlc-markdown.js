0fcee3bfc504a2c8d76adc1e28516de093b1057c
Florent Georges <fgeorges@fgeorges.org>
2019-04-08 13:23:10 +0200

    Handle errors from MarkDown rendering.
    - try auto language detection in case language not known
    - display warning message in case of fatal error processing MarkDown

--- src/js/emlc/emlc-markdown.js
+++ src/js/emlc/emlc-markdown.js
@@ -9,20 +9,40 @@ window.emlc = window.emlc || {};
 
     function renderMarkdown(root, uri) {
         $(document).ready(function () {
-            var renderer = new marked.Renderer();
+            const renderer = new marked.Renderer();
             renderer.image = function(href, title, text) {
                 return '<img src="' + root + 'bin?uri=' + uri + href + '"></img>';
             };
             marked.setOptions({
                 highlight: function(code, lang) {
-                    return lang
-                        ? hljs.highlight(lang, code).value
-                        : hljs.highlightAuto(code).value;
+                    var success = false;
+                    if ( lang ) {
+                        try {
+                            hljs.highlight(lang, code).value;
+                            success = true;
+                        }
+                        catch ( err ) {
+                            console.log('Error highlighting lang ' + lang + ': ' + err);
+                        }
+                    }
+                    if ( ! success ) {
+                        try {
+                            hljs.highlightAuto(code).value;
+                        }
+                        catch ( err ) {
+                            const msg = 'Error highlighting lang ' + lang + ': ' + err;
+                            console.log(msg);
+                            alert(msg + '\nPlease report this to'
+                                  + ' http://github.com/fgeorges/expath-ml-console.'
+                                  + '\nMore info in the browser console logs.');
+                            throw err;
+                        }
+                    }
                 },
                 renderer: renderer
             });
             $('.md-content').each(function() {
-                var elem = $(this);
+                const elem = $(this);
                 elem.html(marked(elem.text()));
             });
         });
