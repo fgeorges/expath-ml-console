6f8ad69ddc909e8dcd059bd84fa331fa49e2f0e6
Florent Georges <fgeorges@fgeorges.org>
2019-04-29 18:06:31 +0200

    Support parameters for XQuery.
    - they need full qualified name
    - they are "parsed" from the query itself (need external variables)
    - passed as clark notation QNames
    
    Still need to:
    
    - support external variable declarations with no type
    - pass type to the eval service (for conversion from string)

--- src/js/emlc/emlc-markdown.js
+++ src/js/emlc/emlc-markdown.js
@@ -60,7 +60,7 @@ window.emlc = window.emlc || {};
         elem.append(pre);
         if ( lang === 'javascript' || lang === 'xquery' ) {
             // add input elements for the params, returns their ID
-            const params = codeParams(elem, token.text, lang);
+            const params = codeParams(elem, token.text, lang, rich);
             // the "target database" selection widget
             const widget = $('#emlc-db-widget-template')
                 .clone()
@@ -87,28 +87,169 @@ window.emlc = window.emlc || {};
         return a.join('');
     }
 
-    function codeParams(elem, text, lang) {
-        const re = lang === 'xquery'
-            ? /^\s*\(:\s*@params\s+(.+\S)\s*:\)\s*\n/
-            : /^\s*\/\/\s*@params\s+(.+\S)\s*\n/;
-        const match = re.exec(text);
-        if ( ! match || ! match[1] ) {
-            return [];
-        }
-        const params = match[1].split(/\s+/);
+    function codeParams(elem, text, lang, rich) {
+        const params = lang === 'xquery' ? codeParamsXqy(rich) : codeParamsJs(text);
         const ids    = [];
         params.forEach(function(p) {
             const id = randomId();
             ids.push(id);
+            // TODO: Make <RET> on these fields to behave like clicking on the "execute" button.
+            // (possible here by using jQuery to add the listener straight away, and use click()
+            // directly on the button itself...?)
             elem.append($(`
                 <div class="form-group row">
-                  <label class="col-sm-2 col-form-label">${p}</label>
+                  <label class="col-sm-2 col-form-label">${p.label}</label>
                   <div class="col-sm-10">
-                    <input type="text" class="form-control" id="${id}" data-param-name="${p}">
+                    <input type="text" class="form-control" id="${id}" data-param-name="${p.name}" title="${p.type}">
                   </div>
                 </div>`));
         });
         return ids;
+    }
+
+    function codeParamsJs(text) {
+        const match = /^\s*\/\/\s*@params\s+(.+\S)\s*\n/.exec(text);
+        if ( ! match || ! match[1] ) {
+            return [];
+        }
+        return match[1].split(/\s+/).map(function(n) { return {name: n, label: n}; });
+    }
+
+    // parse the XQuery code for namespace and variable declarations, based on
+    // the hljs output, used as a poor man's lexer
+    function codeParamsXqy(html) {
+        // parsing util functions
+        const isSpan = function(tok, name) {
+            if ( ! tok ) { return; }
+            return tok.nodeName === 'SPAN'
+                && tok.attributes.class
+                && tok.attributes.class.textContent === ('hljs-' + name);
+        };
+        const isKeyword = function(tok, name) {
+            return isSpan(tok, 'keyword') && tok.innerText === name;
+        };
+        const isLiteral = function(tok) {
+            return isSpan(tok, 'literal') && tok.innerText;
+        };
+        const varName = function(tok) {
+            if ( isSpan(tok, 'variable') && tok.innerText[0] === '$' ) {
+                return tok.innerText.slice(1);
+            }
+        };
+        const isText = function(tok) {
+            if ( ! tok ) { return; }
+            return tok.nodeName === '#text';
+        };
+        const isWs = function(tok) {
+            if ( ! tok ) { return; }
+            return isText(tok) && /^\s+$/.test(tok.textContent);
+        };
+        const next = function(tok) {
+            if ( ! tok ) { return; }
+            return tok.nextSibling;
+        };
+        const nextDecl = function(tok) {
+            if ( ! tok ) { return; }
+            if ( isKeyword(tok, 'declare') ) {
+                return tok;
+            }
+            return nextDecl(next(tok));
+        };
+        const declType = function(tok) {
+            const n = next(tok);
+            if ( n.nodeName !== '#text' ) { return; }
+            if ( /^\s+variable\s+$/.test(n.textContent) ) {
+                return 'variable';
+            }
+            else if ( isWs(n) && isKeyword(next(n), 'namespace') ) {
+                return 'namespace';
+            }
+        };
+        // the variables to accumulate the variable and namespace declarations
+        const vars = [];
+        const nses = {};
+        // "eat" a variable declaration (only external vars are captured)
+        const doVar = function(tok) {
+            const v = {};
+            tok = next(next(tok)); // ignore "declare" and " variable "
+            if ( ! tok ) { return; }
+            v.name = varName(tok);
+            if ( ! v.name ) { return tok; }
+            tok = next(tok);
+            if ( ! isText(tok) ) { return tok; }
+            // TODO: Below, "as" is mandatory, but it is actually optional, even with "external".
+            const match = /^(:[_a-zA-Z][-_.0-9a-zA-Z]*)?\s+$/.exec(tok.textContent);
+            if ( ! match ) { return tok; }
+            if ( match[1] ) {
+                v.prefix = v.name;
+                v.name   = match[1].slice(1);
+            }
+            tok = next(tok);
+            if ( ! isKeyword(tok, 'as') ) { return tok; }
+            v.type = tok.innerText;
+            tok = next(tok);
+            if ( ! isWs(tok) ) { return tok; }
+            tok = next(tok);
+            v.type = isLiteral(tok);
+            if ( ! v.type ) { return tok; }
+            tok = next(tok);
+            if ( ! isText(tok) ) { return tok; }
+            if ( ! /^\s+external\s*;/.test(tok.textContent) ) { return tok; };
+            vars.push(v);
+            return next(tok);
+        };
+        // "eat" a namespace declaration
+        const doNs = function(tok) {
+            tok = next(next(next(tok))); // ignore "declare", " " and "namespace"
+            if ( ! isText(tok) ) { return tok; }
+            const match = /^\s+([_a-zA-Z][-_.0-9a-zA-Z]*)\s*=\s*$/.exec(tok.textContent);
+            if ( ! match ) { return tok; }
+            const prefix = match[1];
+            tok = next(tok);
+            if ( ! isSpan(tok, 'string') ) { return; }
+            const quote = tok.innerText.slice(0, 1);
+            if ( quote !== '"' && quote !== "'" ) { return; }
+            if ( quote !== tok.innerText.slice(-1) ) { return; }
+            const uri = tok.innerText.slice(1, -1);
+            tok = next(tok);
+            if ( ! isText(tok) ) { return tok; }
+            if ( ! /^\s*;/.test(tok.textContent) ) { return tok; };
+            nses[prefix] = uri;
+            return next(tok);
+        };
+        // loop as long as we have "declare" statements
+        const parse = function(tok) {
+            if ( ! tok ) { return; }
+            const decl = nextDecl(tok);
+            if ( ! decl ) { return; }
+            var   here = next(decl);
+            const type = declType(decl);
+            if ( type === 'variable' ) {
+                here = doVar(decl);
+            }
+            else if ( type === 'namespace' ) {
+                here = doNs(decl);
+            }
+            parse(here);
+        };
+        // make actual nodes out of the output of hljs
+        const tokens = $.parseHTML(html);
+        // parse the tokens
+        parse(tokens[0]);
+        return vars.map(function(v) {
+            const res = {
+                type: v.type
+            };
+            if ( v.prefix ) {
+                res.name  = '{' + nses[v.prefix] + '}' + v.name;
+                res.label = v.prefix + ':' + v.name;
+            }
+            else {
+                res.name  = '{}' + v.name;
+                res.label = v.name;
+            }
+            return res;
+        });
     }
 
     // The main function to enrich an element with content from MD tokens.
